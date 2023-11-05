aa6df300b6dc14031a08a48b59bcd9286a2b2107
Sam <samuel.fitz@btinternet.com>
2023-11-05 08:04:48 +0000

    refactor: remove unnecessary html layers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <main>
-      <section className="card">
-        
-      </section>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
